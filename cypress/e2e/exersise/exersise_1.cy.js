348fe5f6424eb4b1d6a75ad54b642c448dcdf9ec
shivareddy <sshivareddy767@example.com>
2022-10-25 20:01:43 +0530

    new changes

--- cypress/e2e/exersise/exersise_1.cy.js
+++ cypress/e2e/exersise/exersise_1.cy.js
@@ -4,7 +4,7 @@ describe('first exersises', () => {
 
     it('eersise_1', () => {
         cy.visit('https://formstone.it/components/dropdown/demo/');
-        cy.wait(5000);
+        cy.wait(5000)
         cy.get('.back_link > a').click();
         cy.xpath('(//a[@class="js-bound"])[1]').click();
         cy.get('[id="demo_label-dropdown-selected"]').click();
